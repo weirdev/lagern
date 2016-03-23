f9e6f78a038fb4811f7f39d0ec5292647a8da8f0
weirdev <weirdev@outlook.com>
2016-03-23 11:54:44 -0500

    Initial commit. Basics in place, metadata not yet stored.

---
+++ Testing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var backupper = new BackupCore.Core(@"C:\Users\Wesley\Desktop\test\src", @"C:\Users\Wesley\Desktop\test\dst");
+            backupper.RunBackup();
+            Console.Out.WriteLine("Done.");
+            Console.In.ReadLine();
+            backupper.ReconstructFile("Testing.docx");
+            Console.Out.WriteLine("Done.");
+            Console.In.ReadLine();
+        }
+    }
+}
