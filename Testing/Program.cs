615a475a76c7c6218d49937bdd29597a2410df70
weirdev <weirdev@outlook.com>
2016-03-24 00:51:10 -0500

    Added BackupCore to repo.

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -11,9 +11,9 @@ namespace Testing
         static void Main(string[] args)
         {
             var backupper = new BackupCore.Core(@"C:\Users\Wesley\Desktop\test\src", @"C:\Users\Wesley\Desktop\test\dst");
-            backupper.RunBackup();
-            Console.Out.WriteLine("Done.");
-            Console.In.ReadLine();
+            //backupper.RunBackup();
+            //Console.Out.WriteLine("Done.");
+            //Console.In.ReadLine();
             backupper.ReconstructFile("Testing.docx");
             Console.Out.WriteLine("Done.");
             Console.In.ReadLine();
